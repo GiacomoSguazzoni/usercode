da0b0fdcee4a727942c9a9371cbfc6507a86a2a0
sguazz <>
2011-03-04 04:00:35 +0000

    Improved copy and merge

--- castorUtils/newCopyAndMerge.py
+++ castorUtils/newCopyAndMerge.py
@@ -3,12 +3,15 @@ import os
 #output
 #targetDir = '/data01/sguazz'
 targetDir = '/raid/sguazz'
+#targetDir = '/tmp/sguazz'
 #targetDir = '/tmp'
 #targetDir = '/afs/cern.ch/user/s/sguazz/scratch1'
 
+tmpCopyDir = '/tmp/sguazz'
+
 rootName = 'ntuple_'
-#rootType = 'conversion_'
-rootType = 'nuclint_'
+rootType = 'conversion_'
+#rootType = 'nuclint_'
 rootSuffix = 'MinBiasMC_'
 #rootSuffix = 'Run2010A_'
 rootExt = '.root'
@@ -18,7 +21,10 @@ numberOfFilesPerChunk = 26
 #castorNumberOfFiles = 270
 #castorNumberOfFiles = 92
 castorNumberOfFiles = 102
-castorPath = '/castor/cern.ch/user/s/sguazz/MaterialNtuples/workDir/MinBiasMC_altTrig_DG/'
+castorPath = '/castor/cern.ch/user/s/sguazz/MaterialNtuples/workDir/MinBiasMC_altTrig_DG_STD/'
+#castorPath = '/castor/cern.ch/user/s/sguazz/MaterialNtuples/workDir/MinBiasMC_altTrig_DG_onlyInGSF2/'
+#castorPath = '/castor/cern.ch/user/s/sguazz/MaterialNtuples/workDir/MinBiasMC_altTrig_DG_inCKFandGSF/'
+###castorPath = '/castor/cern.ch/user/s/sguazz/MaterialNtuples/workDir/MinBiasMC_altTrig_DG/'
 #castorPath = '/castor/cern.ch/user/s/sguazz/MaterialNtuples/workDir/MinBiasMC_altTrig_DG_tinyTest/'
 #castorPath = '/castor/cern.ch/user/s/sguazz/MaterialNtuples/workDir/MinBiasMC_altTrig_noServer/'
 #castorPath = '/castor/cern.ch/user/s/sguazz/MaterialNtuples/workDir/MinBiasMC_altTrig/'
@@ -34,7 +40,9 @@ castorExt = '_1.root'
 addCommand = 'hadd -f'
 #bkg = ' '
 bkg = ' &'
-do = 1
+doCastor = 1
+doViaTMPCopy = 0
+dryRun = 0
 
 ####################################################
 nFile = 0
@@ -48,15 +56,37 @@ while nFile<castorNumberOfFiles :
         nFile = nFile+1
         nInChunk = nInChunk+1
         if nFile<castorNumberOfFiles+1:
-            castorFullPath = castorPath+'/'+castorName+castorType+castorSuffix+str(nFile)+castorExt
-            command = 'stager_get -M '+castorFullPath
-            if do:
-                os.system(command)
+            castorFileName = castorName+castorType+castorSuffix+str(nFile)+castorExt
+            castorFullPath = castorPath+'/'+castorFileName
+            command = 'stager_qry -M '+castorFullPath
             print command
-            mergeCommand = mergeCommand+' rfio:'+castorFullPath
+            check = os.system(command)
+            if check:
+                print 'Skipping non existing file: '+castorFullPath
+            else:
+                if doCastor:
+                    command = 'stager_get -M '+castorFullPath
+                    print command
+                    if not dryRun:
+                        os.system(command)
+                if doViaTMPCopy:
+                    copyFullPath = tmpCopyDir+'/'+castorFileName
+                    command = 'rfcp '+castorFullPath+' '+copyFullPath
+                    print command
+                    copyOk = 0
+                    if not dryRun:
+                        copyOk = os.system(command)
+                    if copyOk:
+                        print ' rfcp failed; skippin in merge: '+castorFullPath
+                    else:
+                        mergeCommand = mergeCommand+' '+copyFullPath
+                else:
+                    mergeCommand = mergeCommand+' rfio:'+castorFullPath
     mergeCommand = mergeCommand+bkg
-    if do:
-        os.system(mergeCommand)
+    print '--------------------------------------------------------'
     print mergeCommand
+    print '--------------------------------------------------------'
+    if not dryRun:
+        os.system(mergeCommand)
 # Fine
 print 'Done.'
